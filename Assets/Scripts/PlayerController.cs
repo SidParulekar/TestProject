7c3be55dc0130993a4f201f9a2d6b68da6ebc4b5
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2024-08-03 16:13:46 +0530

    Implemented Triggering of Game Win and Game Loss
    Created GameObject variables in the script and displayed the appropriate game win or game loss panels.
    
    Created a new game object called enemy which will trigger game loss when player collides with it.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
 
     public Rigidbody2D rigidbody2d;
+
+    public GameObject gameWonPanel;
+
+    public GameObject gameLostPanel;
+
+    private bool isGameOver = false;
 
     public float speed;
 
@@ -18,6 +25,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isGameOver==true)
+        {
+            return;
+        }
+
         if(Input.GetAxis("Horizontal") > 0)
         {
             rigidbody2d.velocity = new Vector2(speed, 0f); 
@@ -44,12 +56,27 @@ public class NewBehaviourScript : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collision.tag == "Door")
+        if(collider.tag == "Door")
         {
             Debug.Log("Level Complete");
+            gameWonPanel.SetActive(true);
+            isGameOver = true;
         }
-        
+
+        else if (collider.tag == "Enemy")
+        {
+            Debug.Log("Game Lost");
+            gameLostPanel.SetActive(true);
+            isGameOver = true;
+        }
+
+    }
+
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("Button Clicked");
     }
 }
